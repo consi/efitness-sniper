7039994e9b203bbc94f922f8fc1b57dfe65858f2
Marek Wajdzik <marek@wajdzik.eu>
2020-03-08 11:52:15 +0100

    Bump version to 0.3 for PyPI upload

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="efitness_sniper",
-    version="0.2",
+    version="0.3",
     install_requires=requirements,
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -21,7 +21,7 @@ setup(
     description="eFitness.com.pl Booking sniper",
     entry_points={"console_scripts": ["efitness=efitness_sniper.app:main"],},
     keywords = ['efitness', 'sniper', 'booking', 'bookings'],
-    download_url= "https://github.com/consi/efitness-sniper/archive/0.2.tar.gz",
+    download_url= "https://github.com/consi/efitness-sniper/archive/0.3.tar.gz",
     classifiers=[
         'Development Status :: 4 - Beta', 
         'Intended Audience :: End Users/Desktop', 
